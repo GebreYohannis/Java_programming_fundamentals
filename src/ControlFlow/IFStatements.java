2d6fee40a9830ba07be5065591c00eb67b9d167f
Demis Getachew <demisgech@gmail.com>
2024-11-16 14:37:22 +0300

    If simple if statement

---
+++ src/ControlFlow/IFStatements.java
@@ -0,0 +1,15 @@
+package ControlFlow;
+
+public class IFStatements {
+    public static void doOperation() {
+        double temperature = 23.2;
+        if(temperature > 30) {
+            System.out.println("It's hot day.");
+            System.out.println("Drink more water.");
+        } else if(temperature >= 20 && temperature <= 30){
+            System.out.println("It's nice day");
+        } else {
+            System.out.println("It's cold.");
+        }
+    }
+}
